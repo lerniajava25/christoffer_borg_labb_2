d91b6f96b579b82cf3135293f7ab980d64693bd7
Christoffer Borg <christofferborg@me.com>
2026-09-02 15:35:14 +0200

    Validate ray direction

--- src/main/java/Ray.java
+++ src/main/java/Ray.java
@@ -3,6 +3,9 @@ public class Ray {
    private final Vector3 direction;
 
    public Ray(Vector3 origin, Vector3 direction) {
+      if (direction.dot(direction) == 0) {
+         throw new IllegalArgumentException("Ray direction cannot be zero");
+      }
       this.origin = origin;
       this.direction = direction;
    }
