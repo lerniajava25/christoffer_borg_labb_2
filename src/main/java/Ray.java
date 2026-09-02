6e2e4d4f7fe878d1181afa2834bfa0f7c097ee05
Christoffer Borg <christofferborg@me.com>
2026-09-02 12:49:39 +0200

    Add Vector3 and Ray domain classes

---
+++ src/main/java/Ray.java
@@ -0,0 +1,11 @@
+public class Ray {
+   private final Vector3 origin;
+   private final Vector3 direction;
+
+   public Ray(Vector3 origin, Vector3 direction) {
+      this.origin = origin;
+      this.direction = direction;
+   }
+
+
+}
---
+++ src/main/java/Vector3.java
@@ -0,0 +1,19 @@
+public class Vector3 {
+    private final double x;
+    private final double y;
+    private final double z;
+
+    public Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector3 subtract(Vector3 other) {
+        return new Vector3(this.x - other.x, this.y - other.y, this.z - other.z);
+    }
+
+    public double dot(Vector3 other) {
+        return this.x * other.x + this.y * other.y + this.z * other.z;
+    }
+}
